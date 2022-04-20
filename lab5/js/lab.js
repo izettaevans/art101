9aa708f9dcf304e9c64e2983e310b2d0d06818a9
izettaevans <ilevans@ucsc.edu>
2022-04-20 15:55:33 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/*
+* Author: Izetta Evans and Maxton Lenox
+* Created: 20 April 2022
+* Assignment: Lab 5
+*/
+
+// Define Varibles
+make = "Toyota";
+model = "Camery";
+color = "Red";
+year = 2006
+
+//calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
